2eb14183be3d3367de1d6e5683d6c38b1e08fb5e
vicky111999 <vigneshsaravanan261@gmail.com>
2025-09-18 13:35:12 +0530

    deploy

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./Chart.css"
+import pic from "../assets/hi.png"
 
 const Name = () => {
   return (
   <>    
-   <div className="name">HI USER<img src='/wave.png' alt='Hi logo'/> </div>
+   <div className="name">HI USER <img src={pic} alt='Hi logo'/></div>
+   
 </>
   )
 }
